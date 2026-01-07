8055718429075742fd34ac9a05c14b92c6db78b3
asdknkwqndknqkfw <codemonkey99999@gmail.com>
2026-01-07 16:06:45 +0900

    feat: 44. APIRouter를 적용하여 업무 도메인별 모듈 분할하기

---
+++ Router/start/routes/user.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+router = APIRouter(prefix="/user", tags=["user"])
+
+@router.get("/")
+async def read_users():
+    return [{"username": "Rickie"}, {"username": "Martin"}]
+
+
+@router.get("/me")
+async def read_user_me():
+    return {"username": "currentuser"}
+
+
+@router.get("/{username}", tags=["users"])
+async def read_user(username: str):
+    return {"username": username}
